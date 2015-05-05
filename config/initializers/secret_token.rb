6e28f8b806ec38dad6801d2b353e8c2dddaf6115
Dhara Joshi <dhara.joshi.hs@gmail.com>
2015-05-05 16:18:36 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMyFirstGem::Application.config.secret_token = '6eed614f58a2d38e7f8b291b3a3b6960c4443b5625b96120fe6b177be0eddd157e1a42ac62e9ed68569612fedc48278248155dd18365c2cf3e71d971e768d386'
